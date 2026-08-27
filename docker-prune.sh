10f2047f5ab1efc90e1f7d97feccdcfc9a8afd0f
Name <e@m.ail>
2026-08-27 10:06:13 +0800

    Remove deprecated docker-compose files for various services

--- docker-prune.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-sudo touch /etc/cron.daily/docker-prune
-
-cat << EOF | sudo tee -a /etc/cron.daily/docker-prune
-#!/bin/bash 
-sudo docker system prune -af  --filter "until=$((30*24))h"
-EOF
